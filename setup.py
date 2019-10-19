881e9f76a6c78584fe941cb5724c2702cc4f0661
cooler <cooler@cz>
2019-10-19 20:43:21 +0800

    :bug: pip version bug

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import sys
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 from os.path import dirname, join
 from setuptools import (
     find_packages,
